efd7a45186777b49d30b168fd14a3e760a6adf21
clivejj <cjohnston1@stuy.edu>
2017-12-12 21:46:46 -0500

    finish

--- find.js
+++ find.js
@@ -13,18 +13,22 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+    var dx = x1 - x0;
+    var dy = y1 - y0;
+    return Math.floor(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
 };
 
 
 var findIt = function(e) {
-  /* YOUR IMPLEMENTATION */
+    var x = event.clientX;
+    var y = event.clientY;
+    var d = distance(x, y, targetX, targetY);
+    console.log(d);
+    console.log(box);
+    box.style.backgroundColor = "rgb(" + d + "," + d + "," + d + ")";
 };
 
-/*
-your OTHER FXNS
 
-*/
 
 box.addEventListener("mousemove", findIt);
 
